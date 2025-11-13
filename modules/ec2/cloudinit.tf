7dab1075d2f1989af43ddf459617a7894c6755be
Alejandro Sajaroff <29068982+asajaroff@users.noreply.github.com>
2025-11-13 08:00:50 +0100

    Adding custom cloudinit feature

--- modules/ec2/cloudinit.tf
+++ modules/ec2/cloudinit.tf
@@ -5,13 +5,13 @@ data "cloudinit_config" "debian" {
   part {
     filename     = "bootstrap-debian.sh"
     content_type = "text/x-shellscript"
-    content      = file("${path.module}/config/bootstrap-debian.sh")
+    content      = var.custom_bootstrap_script != "" ? file(var.custom_bootstrap_script) : file("${path.module}/config/bootstrap-debian.sh")
   }
 
   part {
     filename     = "bootstrap.yaml"
     content_type = "text/cloud-config"
-    content      = file("${path.module}/config/cloud-config-debian.yaml")
+    content      = var.custom_cloud_config != "" ? file(var.custom_cloud_config) : file("${path.module}/config/cloud-config-debian.yaml")
   }
 }
 
@@ -22,13 +22,13 @@ data "cloudinit_config" "ubuntu" {
   part {
     filename     = "bootstrap-ubuntu.sh"
     content_type = "text/x-shellscript"
-    content      = file("${path.module}/config/bootstrap-ubuntu.sh")
+    content      = var.custom_bootstrap_script != "" ? file(var.custom_bootstrap_script) : file("${path.module}/config/bootstrap-ubuntu.sh")
   }
 
   part {
     filename     = "bootstrap.yaml"
     content_type = "text/cloud-config"
-    content      = file("${path.module}/config/cloud-config-ubuntu.yaml")
+    content      = var.custom_cloud_config != "" ? file(var.custom_cloud_config) : file("${path.module}/config/cloud-config-ubuntu.yaml")
   }
 }
 
@@ -39,13 +39,13 @@ data "cloudinit_config" "freebsd" {
   part {
     filename     = "bootstrap-freebsd.sh"
     content_type = "text/x-shellscript"
-    content      = file("${path.module}/config/bootstrap-freebsd.sh")
+    content      = var.custom_bootstrap_script != "" ? file(var.custom_bootstrap_script) : file("${path.module}/config/bootstrap-freebsd.sh")
   }
 
   part {
     filename     = "bootstrap.yaml"
     content_type = "text/cloud-config"
-    content      = file("${path.module}/config/cloud-config-freebsd.yaml")
+    content      = var.custom_cloud_config != "" ? file(var.custom_cloud_config) : file("${path.module}/config/cloud-config-freebsd.yaml")
   }
 }
 
@@ -56,13 +56,13 @@ data "cloudinit_config" "flatcar" {
   part {
     filename     = "bootstrap-flatcar.sh"
     content_type = "text/x-shellscript"
-    content      = file("${path.module}/config/bootstrap-flatcar.sh")
+    content      = var.custom_bootstrap_script != "" ? file(var.custom_bootstrap_script) : file("${path.module}/config/bootstrap-flatcar.sh")
   }
 
   part {
     filename     = "bootstrap.yaml"
     content_type = "text/cloud-config"
-    content      = file("${path.module}/config/cloud-config-flatcar.yaml")
+    content      = var.custom_cloud_config != "" ? file(var.custom_cloud_config) : file("${path.module}/config/cloud-config-flatcar.yaml")
   }
 }
 
